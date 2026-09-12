02bc6782167c030747479b1724f20b71b48d60b7
William Shackelford <bshackelford@gmail.com>
2026-09-12 16:25:53 -0400

    revert: restore runtime forwarding methods

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,34 @@ type EventArguments<T> = T extends unknown[] ? T : never;
 /** Synchronous event bus with optional compile-time event and payload contracts. */
 class Mediator<Events extends object = Record<string | symbol, any[]>> extends EventEmitter {
 
+    override on<Name extends keyof Events & (string | symbol)>(
+        eventName: Name,
+        listener: (...arguments_: EventArguments<Events[Name]>) => void
+    ): this {
+        return super.on(eventName, listener);
+    }
+
+    override once<Name extends keyof Events & (string | symbol)>(
+        eventName: Name,
+        listener: (...arguments_: EventArguments<Events[Name]>) => void
+    ): this {
+        return super.once(eventName, listener);
+    }
+
+    override emit<Name extends keyof Events & (string | symbol)>(
+        eventName: Name,
+        ...arguments_: EventArguments<Events[Name]>
+    ): boolean {
+        return super.emit(eventName, ...arguments_);
+    }
+
+    override removeListener<Name extends keyof Events & (string | symbol)>(
+        eventName: Name,
+        listener: (...arguments_: EventArguments<Events[Name]>) => void
+    ): this {
+        return super.removeListener(eventName, listener);
+    }
+
     /**
      * Create an instance with its own state and listener references.
      */
@@ -41,31 +69,6 @@ class Mediator<Events extends object = Record<string | symbol, any[]>> extends E
     }
 
 };
-
-/** Compile-time event and payload contracts without adding runtime wrappers. */
-interface Mediator<Events extends object = Record<string | symbol, any[]>> {
-
-    on<Name extends keyof Events & (string | symbol)>(
-        eventName: Name,
-        listener: (...arguments_: EventArguments<Events[Name]>) => void
-    ): this;
-
-    once<Name extends keyof Events & (string | symbol)>(
-        eventName: Name,
-        listener: (...arguments_: EventArguments<Events[Name]>) => void
-    ): this;
-
-    emit<Name extends keyof Events & (string | symbol)>(
-        eventName: Name,
-        ...arguments_: EventArguments<Events[Name]>
-    ): boolean;
-
-    removeListener<Name extends keyof Events & (string | symbol)>(
-        eventName: Name,
-        listener: (...arguments_: EventArguments<Events[Name]>) => void
-    ): this;
-
-}
 
 
 
